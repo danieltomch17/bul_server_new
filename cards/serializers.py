378f7f5100139bd631d8c18f53fbf1beed3e18d4
Daniel Tomchin <you@example.com>
2022-04-22 13:34:37 +0300

    update cards serializer

--- cards/serializers.py
+++ cards/serializers.py
@@ -6,4 +6,33 @@ class CardSerializer(serializers.ModelSerializer):
  
     class Meta:
         model = Card
-        fields = ('player_name', 'team_id')
+        fields = ('player_name',
+                'team_id',
+                'pic',
+                'height',
+                'position',
+                'games',
+                'pts',
+                'p21',
+                'p22',
+                'p31',
+                'p32',
+                'p11',
+                'p12',
+                'p2',
+                'p3',
+                'p1',
+                'defensive_reb',
+                'offensive_reb',
+                'total_reb',
+                'fouls_against',
+                'fouls_for',
+                'steals',
+                'turnovers',
+                'assists',
+                'blocks_for',
+                'blocks_against',
+                'VAL',
+                'plusminus',
+                'dunks'
+        )
