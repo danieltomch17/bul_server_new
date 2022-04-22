3aa20568934ccdf5357c3e0e5a0257211ecedb90
Daniel Tomchin <you@example.com>
2022-04-22 12:17:51 +0300

    add default values to Card model

--- cards/models.py
+++ cards/models.py
@@ -4,39 +4,39 @@ from teams.models import Team
 
 # Create your models here.
 class Card(models.Model):
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, default=0)
 
     # player info
-    player_name = models.CharField(max_length=50)
-    pic = models.CharField(max_length=50)
-    height = models.CharField(max_length=50)
-    position = models.CharField(max_length=50)
+    player_name = models.CharField(max_length=50, default=0)
+    pic = models.CharField(max_length=50, default=0)
+    height = models.CharField(max_length=50, default=0)
+    position = models.CharField(max_length=50, default=0)
 
      # player stats
-    games = models.PositiveSmallIntegerField()
-    pts = models.FloatField()
-    p21 = models.FloatField() # avg successfull 2-point shots
-    p22 = models.FloatField() # avg 2-point shots
-    p31 = models.FloatField() # avg successfull 3-point shots
-    p32 = models.FloatField() # avg 3-point shots
-    p11 = models.FloatField() # avg successfull 1-point shots
-    p12 = models.FloatField() # avg 1-point shots
-    p2 = models.FloatField() # avg success rate for 2-point shots
-    p3 = models.FloatField() # avg success rate for 3-point shots
-    p1 = models.FloatField() # avg success rate for 1-point shots
-    defensive_reb = models.FloatField()
-    offensive_reb = models.FloatField()
-    total_reb = models.FloatField()
-    fouls_against = models.FloatField()
-    fouls_for = models.FloatField()
-    steals = models.FloatField()
-    turnovers = models.FloatField()
-    assists = models.FloatField()
-    blocks_for = models.FloatField()
-    blocks_against = models.FloatField()
-    VAL = models.FloatField()
-    plusminus = models.FloatField()
-    dunks = models.FloatField()
+    games = models.PositiveSmallIntegerField(default=0)
+    pts = models.FloatField(default=0)
+    p21 = models.FloatField(default=0) # avg successfull 2-point shots
+    p22 = models.FloatField(default=0) # avg 2-point shots
+    p31 = models.FloatField(default=0) # avg successfull 3-point shots
+    p32 = models.FloatField(default=0) # avg 3-point shots
+    p11 = models.FloatField(default=0) # avg successfull 1-point shots
+    p12 = models.FloatField(default=0) # avg 1-point shots
+    p2 = models.FloatField(default=0) # avg success rate for 2-point shots
+    p3 = models.FloatField(default=0) # avg success rate for 3-point shots
+    p1 = models.FloatField(default=0) # avg success rate for 1-point shots
+    defensive_reb = models.FloatField(default=0)
+    offensive_reb = models.FloatField(default=0)
+    total_reb = models.FloatField(default=0)
+    fouls_against = models.FloatField(default=0)
+    fouls_for = models.FloatField(default=0)
+    steals = models.FloatField(default=0)
+    turnovers = models.FloatField(default=0)
+    assists = models.FloatField(default=0)
+    blocks_for = models.FloatField(default=0)
+    blocks_against = models.FloatField(default=0)
+    VAL = models.FloatField(default=0)
+    plusminus = models.FloatField(default=0)
+    dunks = models.FloatField(default=0)
 
     def __str__(self):
         return self.player_name
