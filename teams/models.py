fee1cc15079d2a62c9348446f0d104215b501a73
Daniel Tomchin <you@example.com>
2022-05-24 01:10:50 +0300

    fix leagus num of teams bug on delete

--- teams/models.py
+++ teams/models.py
@@ -1,6 +1,10 @@
+from re import L
+from django import dispatch
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields.related import ForeignKey
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 from leagues.models import League
 
@@ -15,3 +19,10 @@ class Team(models.Model):
     real_cash = models.CharField(max_length=50, default=0)
     def __str__(self):
         return '({}) {}'.format(self.pk, self.team_name)
+
+@receiver(post_delete, sender=Team, dispatch_uid='team_delete_update_league')
+def update_league_on_team_delete(sender, instance, using, **kwargs):
+    league_to_update_queryset = League.objects.filter(id  = instance.league.id)
+    league_to_update = league_to_update_queryset.first()
+    print(league_to_update)
+    league_to_update_queryset.update(num_of_teams=league_to_update.num_of_teams-1)
